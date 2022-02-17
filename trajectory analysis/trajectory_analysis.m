a2abd58d8d25856fba2b1112549d42e356532f52
Matt Mader <mattmaderCT@gmail.com>
2022-02-17 16:28:02 -0500

    variables for trajectory analysis

--- trajectory analysis/trajectory_analysis.m
+++ trajectory analysis/trajectory_analysis.m
@@ -1,3 +1,26 @@
-function trajectory_analysis
+function [tof,dV] = ...
+    trajectory_analysis(launcher,m_total,orbit,propulsion,flybys)
+% Inputs:
+%   launcher - struct containing launch vehicle information
+%       launcher.type - string for the name, e.g. "NASA SLS"
+%       launcher.masses - array of possible payload mass [kg]
+%       launcher.C3 - array of excess C3 for each payload mass [km^2/s^2]
+%   m_total - estimated total spacecraft mass [kg]
+%
+%   orbit - struct containing final spacecraft orbit
+%       orbit.perihelion - final orbit perihelion [km]
+%       orbit.aphelion - final orbit aphilion [km]
+%       orbit.inclination - final orbit inclination [deg]
+%
+%   propulsion - struct containing propulsion information
+%       propulsion.type - string for the name, e.g. "Solar Sail"
+%
+%   flybys - struct containing flyby information
+%       flybys.planet - string for the name, e.g. "Venus"
+%
+%
+% Ouputs:
+%   tof - time of flight (until final orbit first reached) [days]
+%   dV - delta-V required for propulsion [km/s]
 
 end
