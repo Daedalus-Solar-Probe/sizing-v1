0e5ef3225cdd869325fd4d4b0b59d7332afd4fe0
Matt Mader <mattmaderCT@gmail.com>
2022-02-17 17:30:41 -0500

    inputs/outputs for propulsion sizing

--- propulsion sizing/propulsion_sizing.m
+++ propulsion sizing/propulsion_sizing.m
@@ -1,3 +1,17 @@
-function propulsion_sizing
-
+function [P_prop, m_prop, cost] = propulsion_sizing(dV, propulsion, m_pay)
+% Inputs:
+%   dV - delta-V required for propulsion [km/s]
+%
+%   propulsion - struct containing propulsion information
+%       propulsion.type - string for the name, e.g. "Solar Sail"
+%
+%   m_pay - estimate for the mass of the spacecraft bus + payload [kg]
+%
+%
+% Outputs:
+%   P_prop - Power requirement of the propulsion system [W]
+%
+%   m_prop - Propellant + inert mass for propulsion system [kg]
+%
+%   cost - Cost estimate of propulsion system [USD]
 end
