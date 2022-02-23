d6e85d1aae05d34b7152826994958e8f53ab160f
crawfo80 <nicolas.crawford@gmail.com>
2022-02-23 17:06:54 -0500

    updated cost for spacecraft_bus_sizing

--- spacecraft_bus_sizing/spacecraft_bus_sizing.m
+++ spacecraft_bus_sizing/spacecraft_bus_sizing.m
@@ -11,17 +11,19 @@ function [m_bus, cost] = spacecraft_bus_sizing(payload, P_prop)
 %
 %   cost - estimated cost of payload + spacecraft bus [USD]
 
-% Payload Library [cor, tsi, euvi, dsi, uvs, mag, sw, epp, rpw]
+    %% Definitions
+    
+    % Payload Library [cor, tsi, euvi, dsi, uvs, mag, sw, epp, rpw]
     payLibrary = [10, 7, 10, 25, 15, 1.5, 10, 9, 10; % mass [kg]
         15, 14, 12, 37, 22, 2.5, 15, 9, 15; % power [watts]
         19.3, 13.7, 17.6, 41.5, 26.9, 6.8, 24.3, 17.9, 24.3]; % cost [USD FY22 Millions]
 
-    % Total instrument mass, power, and cost
-    m_inst = 0;p_inst = 0;c_inst = 0;
+    % Total payload mass, power, and cost
+    m_pay = 0;p_pay = 0;c_pay = 0;
     for i = 1:9
-        m_inst = m_inst + payLibrary(1,i)*payload(i);
-        p_inst = p_inst + payLibrary(2,i)*payload(i);
-        c_inst = c_inst + payLibrary(3,i)*payload(i);
+        m_pay = m_pay + payLibrary(1,i)*payload(i); % mass
+        p_pay = p_pay + payLibrary(2,i)*payload(i); % power
+        c_pay = c_pay + payLibrary(3,i)*payload(i); % cost
     end
 
     %Specific power of spacecraft solar panels (w/kg)
@@ -30,19 +32,52 @@ function [m_bus, cost] = spacecraft_bus_sizing(payload, P_prop)
     %Input argument for power source (only considering solar panels for now)
     powersource_specific_power = SolarPanel_specific_power;
 
-    % Additional Mass Required to power instruments (kg)
-    m_power = (p_inst + P_prop) / powersource_specific_power;
+    %% Mass
+    
+    % power mass
+    m_power = (p_pay + P_prop) / powersource_specific_power;
 
     % Bus Mass (kg) [Based upon Michael's estimate equation]
-    m_bus = (m_inst + m_power)/15*100;
+    m_bus = (m_pay + m_power)/15*100;
 
-    % Bus Price
+    %% Cost
+    
+    % Bus Cost
     c_bus = 1.28*(2.835*1000*m_bus^0.716);
+    
+    % Structure/Thermal Cost
+    m_struc = 0;
+    c_struc = 1.28*1000*646*(m_struc)^.684;SSE_struc = .22;
+    c_struc = c_struc*(1 + 0.524*SSE_struc); % 70% certainty
+    
+    % ADCS Cost
+    m_adcs = 0;
+    c_adcs = 1.28*1000*324*(m_adcs);SSE_adcs = .44;
+    c_adcs = c_adcs*(1 + 0.524*SSE_adcs); % 70% certainty
+
+    % EPS Cost
+    m_eps = m_power;
+    c_eps = 1.28*1000*64.3*(m_eps);SSE_eps = .41;
+    c_eps = c_eps*(1 + 0.524*SSE_eps); % 70% certainty
+
+    % RCS Cost
+    vol_rcsTank = 0; % tank volume cc
+    c_rcs = 1.28*1000*20*(vol_rcsTank)^.485;SSE_rcs = .35;
+    c_rcs = c_rcs*(1 + 0.524*SSE_rcs); % 70% certainty
+
+    % TTC Cost
+    c_ttc = 1.28*1000*26916;
+    
+    % Bus + Cost Integration
+    c_int = 1.28*0.195*(c_bus+c_pay);SSE_int = 0.4;
+    c_int = c_int*(1 + 0.524*SSE_int);
 
     % PlaceholderCosts
-    c_comms = 0;
+    m_comm = 0;
+    num_channels = 0;
+    c_comms = 1.28*1000*(339*(m_comm)+5127*(num_channels));
     c_elec = 1.28*1000*64.3*m_bus;
 
-    cost = c_inst + c_bus + c_comms + c_elec;
+    cost = c_pay + c_bus + c_struc + c_adcs + c_eps + c_rcs + c_ttc + c_int + c_comms + c_elec;
 
 end
