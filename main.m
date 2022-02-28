2dc03c75aafc7230d9de4241cef97d32d272fa55
crawfo80 <nicolas.crawford@gmail.com>
2022-02-28 18:22:13 -0500

    updated payload options in main.m with configurations

--- main.m
+++ main.m
@@ -33,12 +33,30 @@ launch_vehicle.mass = [15010, 12345, 10115, 8225, 6640, 5280, 4100, 3080, 2195, 
 
 %% Payload Options
 
-% All instraments
-payload.name = "All";
+% Instraments Library
 payload.sensors = ["COR" "TSI" "EUVI" "DSI" "UVS" "MAG" "SW" "EPP" "RPW"];
 payload.mass = [10, 7, 10, 25, 15, 1.5, 10, 9, 10]; % [kg]
 payload.power = [15, 14, 12, 37, 22, 2.5, 15, 9, 15]; % [W]
 payload.cost = [19.3, 13.7, 17.6, 41.5, 26.9, 6.8, 24.3, 17.9, 24.3]*1e6; % [USD]
+
+% Payload Configuration
+% payload.name = "All";
+% payload.name = "Remote + Mag";
+% payload.name = "In Situ";
+payload.name = "EUVI-DSI-MAG";
+switch payload.name
+    case "All"
+        payload.configuration = [1, 1, 1, 1, 1, 1, 1, 1, 1];
+    case "Remote + Mag"
+        payload.configuration = [1, 1, 1, 1, 1, 1, 0, 0, 0];
+    case "In Situ"
+        payload.configuration = [0, 0, 0, 0, 0, 1, 1, 1, 1];
+    case "EUVI-DSI-MAG"
+        payload.configuration = [0, 0, 1, 1, 0, 1, 0, 0, 0];
+end
+payload.mass = payload.mass .* payload.configuration;
+payload.power = payload.power .* payload.configuration;
+payload.cost = payload.cost .* payload.configuration;
 
 %% Propulsion Options
 
