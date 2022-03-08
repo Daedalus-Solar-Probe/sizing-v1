015a2640fb05dde790e93e2f99bc221ce5912756
Perry158 <perry158@purdue.edu>
2022-03-08 15:35:14 -0500

    Initial study of kick stage performance and effect on launch vehicle and total C3.

---
+++ Kick_Stage.m
@@ -0,0 +1,54 @@
+%% Kick Stage
+function [dV,Cost,C3_old,C3_kick,C3_lv,C3_new] = Kick_Stage (mass,launch_vehicle,type)
+
+g0 = 9.81;
+sc_mass = mass.total;
+% sc_mass = 160; % kg
+
+if type == "none"
+    mass_i = 0; % kg
+    dV = 0; % km/s
+    C3_kick = dV^2; % km^2/s^2
+    Cost = 0; % $
+elseif type == "Star 27H"
+    Isp = 291.4; % s
+    mass_i = 367.8; % kg
+    mass_f = mass_i - 338.8; % kg
+    dV = Isp*g0*log((sc_mass+mass_i)/(sc_mass+mass_f))/1000; % km/s
+    C3_kick = dV^2; % km^2/s^2
+    Cost = 1; % $
+elseif type == "Star 30E"
+    Isp = 290.4; % s
+    mass_i = 673.9; % kg
+    mass_f = mass_i - 631.4; % kg
+    dV = Isp*g0*log((sc_mass+mass_i)/(sc_mass+mass_f))/1000; % km/s
+    C3_kick = dV^2; % km^2/s^2
+    Cost = 1.5; % $
+elseif type == "Star 48BV"
+    Isp = 292.1; % s
+    mass_i = 2164.5; % kg
+    mass_f = mass_i - 2010; % kg
+    dV = Isp*g0*log((sc_mass+mass_i)/(sc_mass+mass_f))/1000; % km/s
+    C3_kick = dV^2; % km^2/s^2
+    Cost = 2; % $
+elseif type == "Castor-30XL"
+    Isp = 294.4; % s
+    mass_i = 26407; % kg
+    mass_f = mass_i - 24925; % kg
+    dV = Isp*g0*log((sc_mass+mass_i)/(sc_mass+mass_f))/1000; % km/s
+    C3_kick = dV^2; % km^2/s^2
+    Cost = 3; % $
+end
+
+%% Check LV C3
+% Before kick stage
+C3_old = interp1(launch_vehicle.mass,launch_vehicle.C3,sc_mass,'linear','extrap'); % [km^2/s^2]
+
+% After kick stage
+mass_new = sc_mass + mass_i; % kg
+C3_lv = interp1(launch_vehicle.mass,launch_vehicle.C3,mass_new,'linear','extrap'); % [km^2/s^2]
+C3_new = C3_lv + C3_kick; % km^2/s^2
+
+
+
+end
